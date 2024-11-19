3c0242fd6862d58184ebb269e236c8def4f071d2
Ram Rijal <87277221+RamRijal@users.noreply.github.com>
2024-11-19 11:43:29 +0545

    folders added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "@/components/dashboard/Navbar";
+import SideBar from "@/components/dashboard/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
+        <SideBar />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import NavBar from "@/components/Navbar";
-import SideBar from "@/components/Sidebar";
 
 export default function Home() {
   return (
     <>
-      <NavBar />
-      <SideBar />
     </>
   );
 }
