07f7cac61a585b3c021aa105c32ba0ade5c7bb7a
Ram Rijal <87277221+RamRijal@users.noreply.github.com>
2024-11-21 14:32:51 +0545

    NavBar RHS buttons update

--- components/dashboard/NavBar.tsx
+++ components/dashboard/NavBar.tsx
@@ -1,6 +1,40 @@
-import { Bell, Moon, User } from 'lucide-react'
+'use client'
+import { Bell, LogOutIcon, Moon, User } from 'lucide-react'
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 
 const NavBar = () => {
+    const [isUserToggled, setIsUserToggled] = useState(false);
+    const [isNotificationsToggled, setIsNotificationsToggled] = useState(false);
+    const userMenuRef = useRef<HTMLDivElement>(null);
+
+    const toggleUser = () => {
+        setIsUserToggled(!isUserToggled);
+    }
+    const toggleNotifications = () => {
+        setIsNotificationsToggled(!isNotificationsToggled);
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                userMenuRef.current &&
+                event.target instanceof Node &&
+                !userMenuRef.current.contains(event.target)
+            ) {
+                setIsUserToggled(false);
+            }
+        };
+
+        // Type casting for addEventListener
+        const handleClick = handleClickOutside as (event: MouseEvent) => void;
+
+        document.addEventListener('mousedown', handleClick);
+        return () => {
+            document.removeEventListener('mousedown', handleClick);
+        };
+    }, []);
+
     return (
         <header className="sticky top-0 z-50 flex items-center justify-between px-6 py-4 bg-white border-b">
             <div className="flex w-full gap-7 items-center justify-left">
@@ -24,20 +58,86 @@ const NavBar = () => {
                     </div>
                 </form>
             </div>
-
+            {/* RHS of navbar */}
             <div className="flex items-center space-x-2">
                 <div className="cursor-pointer border-none hover:rounded-full hover:bg-active-gray p-2">
                     <Moon className='text-nav-icon ' size={20} />
                 </div>
-                <div className="cursor-pointer border-none hover:rounded-full hover:bg-active-gray p-2">
-                    <Bell className='text-nav-icon ' size={20} />
+                {/* MOBILE -Notifications*/}
+                <div
+                    ref={userMenuRef}
+                    className="relative"
+                >
+                    <div
+                        onClick={toggleNotifications}
+                        className="flex relative items-center  space-x-2 border-none hover:rounded-full hover:bg-active-gray focus:scale-50 cursor-pointer">
+                        <div className="cursor-pointer border-none hover:rounded-full hover:bg-active-gray focus:bg-active-gray p-2">
+                            <Bell className='text-nav-icon ' size={20} />
+                        </div>
+                    </div>
+                    {isNotificationsToggled &&
+                        <div className="absolute right-0 top-full mt-2 z-10 bg-white border border-active-gray shadow-lg min-w-96 py-2 px-2 rounded-md  "
+                            role="menu">
+                            <div className="px-2">
+                                <p className="px-2 py-2 text-lg font-medium text-gray-600"> Notifications</p></div>
+                            <ul className=" text-xs text-gray-700 dark:text-gray-400" >
+                                <li className='px-2  rounded-md border-b-2 border-active-gray  dark:hover:text-white'>
+                                    <p className="block py-2 px-2  rounded-md text-inactive-title ">Hello World Hello World Hello World Hello World Hello World</p>
+                                </li>
+                                <li className='px-2  rounded-md border-b-2 border-active-gray  dark:hover:text-white'>
+                                    <p className="block py-2 px-2  rounded-md text-inactive-title ">Hello World Hello World Hello World Hello World Hello World</p>
+                                </li>
+                                <li className='px-2 rounded-md dark:hover:text-white'>
+                                    <p className="block py-2 px-2  rounded-md text-inactive-title ">Hello World Hello World Hello World Hello World </p>
+                                </li>
+
+                            </ul>
+                        </div>
+                    }
                 </div>
-                <div className="flex px-2 space-x-2 border-none hover:rounded-full hover:bg-active-gray hover:py-2 focus:scale-50 cursor-pointer">
-                    <User className='text-nav-icon ' size={20} />
-                    <span className='hidden md:block text-nav-icon text-sm '>Username</span>
+
+                {/* MOBILE - USER */}
+                <div
+                    ref={userMenuRef}
+                    className="relative"
+                >
+                    <div
+                        onClick={toggleUser}
+                        className="flex relative items-center px-2 space-x-2 border-none hover:rounded-full hover:bg-active-gray focus:scale-50 cursor-pointer">
+                        <div className="flex px-1 py-2 gap-1">
+                            <User className='text-nav-icon ' size={20} />
+                            <span className='hidden md:block text-nav-icon text-base prevent-select'>Username</span>
+                        </div>
+                    </div>
+                    {isUserToggled &&
+                        <div className="absolute right-0 top-full mt-2 z-10 bg-white border border-active-gray shadow-lg min-w-56  px-2 rounded-md  "
+                            role="menu">
+                            <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" >
+                                <li className='px-2 pb-3 rounded-md border-b-2 border-active-gray  dark:hover:text-white'>
+                                    <Link href="#" className="block pt-2 text-black">Username</Link>
+                                    <p className=' text-xs text-inactive-title'>username@gmail.com</p>
+                                </li>
+                                <li>
+                                    <Link href="#" className="block py-2 px-2  rounded-md text-black hover:bg-active-gray">Profile</Link>
+                                </li>
+                                <li>
+                                    <Link href="#" className="block  py-2 px-2 text-black rounded-md  hover:bg-active-gray">Settings</Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        className="flex py-2 px-2  rounded-md text-black items-center border-t-2 border-active-gray gap-2 hover:bg-active-gray"
+                                        role="menuitem" href={''}>
+                                        <LogOutIcon size={18} />
+                                        Logout
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    }
                 </div>
             </div>
-        </header>
+
+        </header >
     )
 }
 
