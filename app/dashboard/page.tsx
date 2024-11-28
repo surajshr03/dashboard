c70d379c02cccb3bfed16e12a53f8a565aa24584
Suraj Shrestha <suraj.shr03@gmail.com>
2024-11-28 16:19:08 +0545

    BreadCrumbs added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,22 +2,14 @@ import CustomChart from '@/components/dashboard/GraphData/CustomChart'
 import Metrics from '@/components/dashboard/Metrics'
 import RecentActivities from '@/components/dashboard/RecentActivities'
 import Link from 'next/link'
+import Breadcrumbs from './Breadcrumbs/BreadCrumbs'
 
 const Dashboard = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row gap-2 justify-between">
         <p className='text-3xl font-medium text-darkest-inactive-title'>Dashboard Overview</p >
-        {/* BREADCRUMBS */}
-        <div className="flex gap-2">
-          <Link href="/">
-            <span className="text-sm text-inactive-title font-medium ">Home &gt;</span>
-          </Link>
-          <Link href="/dashboard">
-            <span className="text-sm text-darkest-inactive-title font-medium">Dashboard </span>
-          </Link>
-         
-        </div>
+        <Breadcrumbs/>
       </div>
       <Metrics />
       <CustomChart/>
