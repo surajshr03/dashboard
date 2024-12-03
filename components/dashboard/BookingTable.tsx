3b92f78b914b4f621cc89f7f8391968b49ff4176
Ram Rijal <87277221+RamRijal@users.noreply.github.com>
2024-12-03 14:34:23 +0545

    minor updates

--- components/dashboard/BookingTable.tsx
+++ components/dashboard/BookingTable.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Eye, Search, Trash2, UserRoundPen } from "lucide-react";
-import { SetStateAction, useState } from "react";
+import { Cross, Eye, Search, Trash2, UserRoundPen, X } from "lucide-react";
+import { SetStateAction, useEffect, useState } from "react";
 import "@/components/dashboard/CSS/dashboard.css";
 import { Bookings } from "@/data/data";
 import { BookingProps } from "@/data/type";
@@ -12,7 +12,8 @@ const BookingTable = () => {
   const [searchQuery, setSearchQuery] = useState(""); // Search state
   const [filterDateRange, setFilterDateRange] = useState("All"); // State for selected date range filter
 
- const [selectedBooking, setSelectedBooking]= useState<BookingProps | null>(null);
+  const [deleteSelected, setDeleteSelected] = useState<BookingProps | null>(null);
+  const [selectedBooking, setSelectedBooking] = useState<BookingProps | null>(null);
 
   // Toggle filter dropdown
   const toggleFilter = () => {
@@ -36,12 +37,34 @@ const BookingTable = () => {
   };
 
   //function for popup detail display and close it
-  const handleViewDetails = (booking:BookingProps)=>{
-   setSelectedBooking(booking);
+  const handleViewDetails = (booking: BookingProps) => {
+    setSelectedBooking(booking);
   };
-  const closePopup =()=>{
+  const handleDelete = (booking: BookingProps) => {
+    setDeleteSelected(booking);
+  };
+  const closeDeletePopup = () => {
+    setDeleteSelected(null);
+  }
+  const closePopup = () => {
     setSelectedBooking(null);
   }
+  // Close popup on Escape key press
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        closePopup();
+        closeDeletePopup();
+      }
+    };
+    if (deleteSelected) {
+      window.addEventListener("keydown", handleEscape);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleEscape);
+    };
+  }, [deleteSelected]);
 
   // Function to filter bookings based on selected filter (status, date range) and search query
   const filteredBookings = Bookings.filter((booking) => {
@@ -78,88 +101,88 @@ const BookingTable = () => {
       <h5 className="title">Manage Bookings</h5>
       <h6 className="sub-title">Manage and view bookings</h6>
 
-     
+
       {/* Search and Filter Section */}
-        <div className="grid grid-cols-6 gap-4 my-6">
+      <div className="grid grid-cols-6 gap-4 my-6">
 
-          {/* Search Input */}
-          <div className="col-span-6 lg:col-span-4 relative">
-            <input
-              type="text"
-              placeholder="Search bookings..."
-              value={searchQuery}
-              onChange={handleSearchChange}
-              className="p-2 pl-10 pr-4 border rounded-md w-full bg-gray-100"
-            />
-            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
-              <Search />
-            </span>
-          </div>
-
-          {/* Date Range Filter */}
-          <div className="col-span-6 md:col-span-3 lg:col-span-1  relative">
-            <select
-              value={filterDateRange}
-              onChange={handleDateRangeChange}
-              className="p-2 border rounded-md w-full bg-gray-100"
-            >
-              <option value="All">All Dates</option>
-              <option value="Before 2010">Before 2010</option>
-              <option value="2011-2015">2011-2015</option>
-              <option value="2016-2020">2016-2020</option>
-              <option value="2021-2025">2021-2025</option>
-              <option value="2026-2030">2026-2030</option>
-            </select>
-          </div>
-
-          {/* Filter Button */}
-          <div className="col-span-6 md:col-span-3 lg:col-span-1 flex justify-end relative">
-            <button
-              className="filter-btn flex items-center justify-start px-4 py-2 border w-full"
-              onClick={toggleFilter}
-            >
-              {/* Filter label */}
-              <p className="font-bold text-white">Filter</p>
-              {/* Selected status */}
-              <p className="ml-2 text-sm font-light text-white">
-                (&nbsp;{filterStatus}&nbsp;)
-              </p>
-            </button>
-
-            {/* Filter Dropdown */}
-            {isFilterOpen && (
-              <div className="absolute right-0 mt-2 bg-white border rounded shadow-lg z-10">
-                <ul className="py-2">
-                  <li
-                    className="px-4 py-2 cursor-pointer hover:bg-gray-200"
-                    onClick={() => handleFilterChange("All")}
-                  >
-                    All
-                  </li>
-                  <li
-                    className="px-4 py-2 cursor-pointer hover:bg-gray-200"
-                    onClick={() => handleFilterChange("Confirmed")}
-                  >
-                    Confirmed
-                  </li>
-                  <li
-                    className="px-4 py-2 cursor-pointer hover:bg-gray-200"
-                    onClick={() => handleFilterChange("Pending")}
-                  >
-                    Pending
-                  </li>
-                  <li
-                    className="px-4 py-2 cursor-pointer hover:bg-gray-200"
-                    onClick={() => handleFilterChange("Canceled")}
-                  >
-                    Canceled
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-
+        {/* Search Input */}
+        <div className="col-span-6 lg:col-span-4 relative">
+          <input
+            type="text"
+            placeholder="Search bookings..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+            className="p-2 pl-10 pr-4 border rounded-md w-full bg-gray-100"
+          />
+          <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
+            <Search />
+          </span>
         </div>
+
+        {/* Date Range Filter */}
+        <div className="col-span-6 md:col-span-3 lg:col-span-1  relative">
+          <select
+            value={filterDateRange}
+            onChange={handleDateRangeChange}
+            className="p-2 border rounded-md w-full bg-gray-100"
+          >
+            <option value="All">All Dates</option>
+            <option value="Before 2010">Before 2010</option>
+            <option value="2011-2015">2011-2015</option>
+            <option value="2016-2020">2016-2020</option>
+            <option value="2021-2025">2021-2025</option>
+            <option value="2026-2030">2026-2030</option>
+          </select>
+        </div>
+
+        {/* Filter Button */}
+        <div className="col-span-6 md:col-span-3 lg:col-span-1 flex justify-end relative">
+          <button
+            className="filter-btn flex items-center justify-start px-4 py-2 border w-full"
+            onClick={toggleFilter}
+          >
+            {/* Filter label */}
+            <p className="font-bold text-white">Filter</p>
+            {/* Selected status */}
+            <p className="ml-2 text-sm font-light text-white">
+              (&nbsp;{filterStatus}&nbsp;)
+            </p>
+          </button>
+
+          {/* Filter Dropdown */}
+          {isFilterOpen && (
+            <div className="absolute right-0 mt-2 bg-white border rounded shadow-lg z-10">
+              <ul className="py-2">
+                <li
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-200"
+                  onClick={() => handleFilterChange("All")}
+                >
+                  All
+                </li>
+                <li
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-200"
+                  onClick={() => handleFilterChange("Confirmed")}
+                >
+                  Confirmed
+                </li>
+                <li
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-200"
+                  onClick={() => handleFilterChange("Pending")}
+                >
+                  Pending
+                </li>
+                <li
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-200"
+                  onClick={() => handleFilterChange("Canceled")}
+                >
+                  Canceled
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+      </div>
 
       {/* Table */}
       <div className="overflow-x-auto max-w-full custom-scrollbar">
@@ -186,28 +209,27 @@ const BookingTable = () => {
                   <td className="p-2 text-dark-inactive-title">
                     {booking.date}
                   </td>
-                  <td className="p-2 text-dark-inactive-title">
+                  <td className="p-2 text-dark-inactive-title ">
                     <span
-                      className={`rounded-full text-white ${
-                        booking.status.toLowerCase() === "confirmed"
-                          ? "bg-btn-confirmed"
-                          : booking.status.toLowerCase() === "pending"
+                      className={`rounded-full w-full text-sm ${booking.status.toLowerCase() === "confirmed"
+                        ? "bg-btn-confirmed"
+                        : booking.status.toLowerCase() === "pending"
                           ? "bg-btn-pending"
                           : "bg-btn-canceled"
-                      }`}
+                        }`}
                     >
                       {booking.status}
                     </span>
                   </td>
                   <td className="p-2 flex space-x-2 justify-between">
                     <button
-                    onClick={()=>{
-                      handleViewDetails(booking)
-                    }}
+                      onClick={() => {
+                        handleViewDetails(booking)
+                      }}
                       className="hover:cursor-pointer text-gray-500 group flex items-center space-x-1"
                       title="Detail"
                     >
-                      <span className="group-hover:text-gray-700 group-hover:scale-125 transition-transform duration-200 ease-in-out">
+                      <span className="group-hover:text-gray-600 group-hover:scale-100 transition-transform duration-200 ease-in-out">
                         <Eye />
                       </span>
                       {/* <span className="hidden lg:block group-hover:text-gray-700 transition-colors duration-200 ease-in-out">
@@ -218,7 +240,7 @@ const BookingTable = () => {
                       className="hover:cursor-pointer text-green-500 group flex items-center space-x-1"
                       title="Edit"
                     >
-                      <span className="group-hover:text-green-700 group-hover:scale-125 transition-transform duration-200 ease-in-out">
+                      <span className="group-hover:text-green-600 group-hover:scale-100 transition-transform duration-200 ease-in-out">
                         <UserRoundPen />
                       </span>
                       {/* <span className="hidden lg:block group-hover:text-green-700 transition-colors duration-200 ease-in-out">
@@ -229,8 +251,9 @@ const BookingTable = () => {
                     <button
                       className="hover:cursor-pointer text-red-500 group flex items-center space-x-1"
                       title="Delete"
+                      onClick={() => handleDelete(booking)}
                     >
-                      <span className="group-hover:text-red-700 group-hover:scale-125 transition-transform duration-200 ease-in-out">
+                      <span className="group-hover:text-red-600 group-hover:scale-100 transition-transform duration-200 ease-in-out">
                         <Trash2 />
                       </span>
                       {/* <span className="hidden lg:block group-hover:text-red-700 transition-colors duration-200 ease-in-out">
@@ -242,7 +265,7 @@ const BookingTable = () => {
               ))
             ) : (
               <tr>
-                <td colSpan="5" className="p-2 text-center">
+                <td className="p-2 text-center">
                   No bookings found.
                 </td>
               </tr>
@@ -250,46 +273,62 @@ const BookingTable = () => {
           </tbody>
         </table>
       </div>
-      
+
       {/* View Detail PopUp */}
-      {selectedBooking && 
-      <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="flex flex-col bg-white p-6 rounded-md shadow-lg w-96 gap-4">
-        <h2 className="text-2xl font-bold mb-4 text-center">
-          User Details
-        </h2>
-        <div className="flex flex-col">
-          <strong className="text-lg">ID:</strong>
-          <p>{selectedBooking.id}</p>
-        </div>
-        <div className="flex flex-col">
-          <strong className="text-lg">Customer Name:</strong>
-          <p> {selectedBooking.name}</p>
-        </div>
+      {selectedBooking &&
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="flex flex-col bg-white p-6 rounded-md shadow-lg w-96 gap-4">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              User Details
+            </h2>
+            <div className="flex flex-col">
+              <strong className="text-lg">ID:</strong>
+              <p>{selectedBooking.id}</p>
+            </div>
+            <div className="flex flex-col">
+              <strong className="text-lg">Customer Name:</strong>
+              <p> {selectedBooking.name}</p>
+            </div>
 
-        <div className="flex flex-col">
-          <strong className="text-lg">Book:</strong>
-          <p>{selectedBooking.book}</p>
-        </div>
-        <div className="flex flex-col">
-          <strong className="text-lg">Date:</strong>
-          <p>{selectedBooking.date}</p>
-        </div>
-        <div className="flex flex-col">
-          <strong className="text-lg">Status:</strong>
-          <p>{selectedBooking.status}</p>
-        </div>
+            <div className="flex flex-col">
+              <strong className="text-lg">Book:</strong>
+              <p>{selectedBooking.book}</p>
+            </div>
+            <div className="flex flex-col">
+              <strong className="text-lg">Date:</strong>
+              <p>{selectedBooking.date}</p>
+            </div>
+            <div className="flex flex-col">
+              <strong className="text-lg">Status:</strong>
+              <p>{selectedBooking.status}</p>
+            </div>
 
-        <div className="mt-6 flex flex-col justify-center items-center">
-          <button
-            className=" px-4 py-2  bg-gray-300 text-gray-800 rounded-md"
-            onClick={closePopup}
-          >
-            Close
-          </button>
+            <div className="mt-6 flex flex-col justify-center items-center">
+              <button
+                className=" px-4 py-2  bg-gray-300 text-gray-800 rounded-md"
+                onClick={closePopup}
+              >
+                Close
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      }
+      {deleteSelected &&
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="flex flex-col bg-white p-6 rounded-md shadow-lg w-96 gap-4">
+            <h2 className="text-2xl  text-center cursor-pointer justify-end flex text-inactive-title font-semibold" onClick={closeDeletePopup}><X /></h2>
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Are you sure you want to delete this entry?
+            </h2>
+            <div className="flex flex-col md:flex-row justify-center items-center gap-8">
+              <button onClick={closeDeletePopup} className="bg-metricsRed p-3 py-2 w-28 rounded-sm text-lg text-white">Delete</button>
+              <button onClick={closeDeletePopup} className="bg-inactive-title p-3 py-2 w-28 rounded-sm text-lg text-white">Cancel</button>
+            </div>
+
+
+          </div>
+        </div>
       }
     </div>
   );
