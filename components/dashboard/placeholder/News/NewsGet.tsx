8b86daa7a65b055db3bd279cc0182b413da67910
suraj.sh03 <suraj.shr03@gmail.com>
2024-12-31 14:34:20 +0545

    newsget component update

--- components/dashboard/placeholder/News/NewsGet.tsx
+++ components/dashboard/placeholder/News/NewsGet.tsx
@@ -13,20 +13,22 @@ interface NewsItem{
     content: string;
     category: string;
     url: string;
-    date: string;
+    created_at: string;
     summary: string;
 }
 
 const NewsGet = () => {
   const [news, setNews] = useState<NewsItem[]>([]);
   const [error, setError] = useState<string | null>(null); // Allow null for no errors
+  const [currentPage, setCurrentPage] = useState(1);
+
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://192.168.1.77:8000/news/news/");
         // console.log(response);
-        console.log(response.data);
+        // console.log(response.data);
 
         setNews(response.data);
       } catch (error) {
@@ -47,8 +49,7 @@ const NewsGet = () => {
     setCurrentPage(page);
   };
 
-   const pageSize = 3;
-    const [currentPage, setCurrentPage] = useState(1);
+   const pageSize = 5;
     const startIndex = (currentPage - 1) * pageSize;
     const currentItems = news.slice(startIndex, startIndex + pageSize);
 
@@ -82,7 +83,7 @@ const NewsGet = () => {
                   <td className="p-2 text-dark-inactive-title">{n.content}</td>
                   <td className="p-2 text-dark-inactive-title">{n.category}</td>
                   <td className="p-2 text-dark-inactive-title">{n.url}</td>
-                  <td className="p-2 text-dark-inactive-title">{n.date}</td>
+                  <td className="p-2 text-dark-inactive-title">{new Date(n.created_at).toLocaleDateString()}</td>
                   <td className="p-2 text-dark-inactive-title">{n.summary}</td>
                 </tr>
                     )
@@ -95,17 +96,17 @@ const NewsGet = () => {
               )}
             </tbody>
           </table>
-        </div>
-          
-        <div className="w-full">
+          <div className="w-full">
             <Pagination
-              items={news.length} // 100
+              items={news.length} 
               currentPage={currentPage} // 1
               pageSize={pageSize} // 10
               onPageChange={onPageChange}
             />
           </div>
 
+        </div>
+          
       </div>
     </>
   );
